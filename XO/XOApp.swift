049506459d575ee0f3f6d0f80669b1067d8dc055
long <long.contact@icloud.com>
2026-06-29 16:38:16 +0700

    chore: scaffold XO iOS project with test target

---
+++ XO/XOApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct XOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("XO")
+        }
+    }
+}
---
+++ XOTests/XOTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import XO
+
+final class XOTests: XCTestCase {
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
